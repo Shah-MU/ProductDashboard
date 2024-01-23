b2c4994305b2cb03d3761ef70135d49d180a6f61
Kawasaki_314 <38872709+Kawasaki314@users.noreply.github.com>
2024-01-23 04:14:28 -0500

    Update main.py
    Also changed product text

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ if grid_table['selected_rows']:
 selected_row = None
 
 if not grid_table['selected_rows']:
-        st.info('Select a product to view more info.')
+        st.info('A product must be selected.')
 
 if grid_table['selected_rows']:
     selected_row = grid_table['selected_rows'][0]
